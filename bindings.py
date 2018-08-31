8005db0ee962523a9ef1433fe75b0af706bc3dba
Vladimir Shkoda <vladimir.shkoda.51@gmail.com>
2018-08-31 16:48:37 +0300

    Remove context manager calls from redis usage.

--- bindings.py
+++ bindings.py
@@ -9,57 +9,49 @@ class RedisList(object):
     """
     def __init__(self, redis, key_name, *values):
         self.redis = redis
-        with self.redis() as redis:
-            if values:
-                redis.delete(key_name)
-                redis.rpush(key_name, *values)
-            else:
-                key_type = redis.type(key_name)
-                if key_type not in (REDIS_TYPE_LIST, REDIS_TYPE_NONE):
-                    raise TypeError('Cannot bind to "{0}"'.format(key_type))
+        if values:
+            self.redis.delete(key_name)
+            self.redis.rpush(key_name, *values)
+        else:
+            key_type = self.redis.type(key_name)
+            if key_type not in (REDIS_TYPE_LIST, REDIS_TYPE_NONE):
+                raise TypeError('Cannot bind to "{0}"'.format(key_type))
         self.key_name = key_name
+
+    @property
+    def values(self):
+        """ Return whole list from Redis """
+        return self.redis.lrange(self.key_name, 0, -1)
 
     def append(self, value):
         """ Append value to the end of list """
-        with self.redis() as redis:
-            redis.rpush(self.key_name, value)
+        self.redis.rpush(self.key_name, value)
 
     def extend(self, iterable):
         """ Extend list by appending elements from the iterable """
-        with self.redis() as redis:
-            redis.rpush(self.key_name, *iterable)
+        self.redis.rpush(self.key_name, *iterable)
 
     def remove(self, value):
         """
         L.remove(value) -- remove first occurrence of value.
         Raises ValueError if the value is not present.
         """
-        with self.redis() as redis:
-            if not redis.lrem(self.key_name, value, 1):
-                raise ValueError('value not in list')
-
-    @property
-    def values(self):
-        """ Return whole list from Redis """
-        with self.redis() as redis:
-            return redis.lrange(self.key_name, 0, -1)
+        if not self.redis.lrem(self.key_name, value, 1):
+            raise ValueError('value not in list')
 
     def __getitem__(self, index):
         """ x.__getitem__(y) <==> x[y] """
         if isinstance(index, int):
-            with self.redis() as redis:
-                return redis.lindex(self.key_name, index)
+            return self.redis.lindex(self.key_name, index)
         elif isinstance(index, slice):
             start, stop, step = index.indices(len(self))
-            with self.redis() as redis:
-                return redis.lrange(self.key_name, start, stop)
+            return self.redis.lrange(self.key_name, start, stop)
         else:
             raise TypeError('Invalid index type')
 
     def __len__(self):
         """ x.__len__() <==> len(x) """
-        with self.redis() as redis:
-            return redis.llen(self.key_name)
+        return self.redis.llen(self.key_name)
 
     def __iter__(self):
         """ x.__iter__() <==> iter(x) """
@@ -68,4 +60,4 @@ class RedisList(object):
 
     def __repr__(self):
         """ x.__repr__() <==> repr(x) """
-        return "{0}: {1}".format(self.__class__.__name__, self.values)
+        return '{0}: {1}'.format(self.__class__.__name__, self.values)
