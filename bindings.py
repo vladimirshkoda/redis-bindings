d0d894c38e274235cca8f98b4dbd2ae24738dbff
Vladimir Shkoda <vladimir.shkoda.51@gmail.com>
2018-09-01 20:50:45 +0300

    Add values pickling.

--- bindings.py
+++ bindings.py
@@ -1,34 +1,67 @@
+import cPickle
+import collections
+
+from functools import partial
+
+
 REDIS_TYPE_LIST = 'list'
 REDIS_TYPE_NONE = 'none'
+
+
+def loads(value):
+    """
+    Unpickles value, raises a ValueError in case anything fails.
+    """
+    try:
+        obj = cPickle.loads(value)
+    except Exception as e:
+        raise ValueError('Cannot unpickle value', e)
+    return obj
+
+
+# Serialize pickle dumps using the highest pickle protocol (binary, by default uses ascii)
+dumps = partial(cPickle.dumps, protocol=cPickle.HIGHEST_PROTOCOL)
 
 
 class RedisList(object):
     """
     Provides basic list bindings for Redis list type. Visit https://redis.io/commands#list
-    to have better understanding. All values will be casted to STRINGS.
+    to have better understanding.
     """
-    def __init__(self, redis, key_name, *values):
-        self.redis = redis
-        if values:
+    def __init__(self, redis_connection, key_name, iterable=None, pickling=True):
+        self.redis = redis_connection
+        if iterable:
+            if not isinstance(iterable, collections.Iterable):
+                raise ValueError('values are not iterable')
             self.redis.delete(key_name)
-            self.redis.rpush(key_name, *values)
+            if pickling:
+                iterable = map(dumps, iterable)
+            self.redis.rpush(key_name, *iterable)
         else:
             key_type = self.redis.type(key_name)
             if key_type not in (REDIS_TYPE_LIST, REDIS_TYPE_NONE):
                 raise TypeError('Cannot bind to "{0}"'.format(key_type))
         self.key_name = key_name
+        self.pickling = pickling
 
     @property
     def values(self):
         """ Return whole list from Redis """
-        return self.redis.lrange(self.key_name, 0, -1)
+        values = self.redis.lrange(self.key_name, 0, -1)
+        if self.pickling:
+            values = map(loads, values)
+        return values
 
     def append(self, value):
         """ Append value to the end of list """
+        if self.pickling:
+            value = dumps(value)
         self.redis.rpush(self.key_name, value)
 
     def extend(self, iterable):
         """ Extend list by appending elements from the iterable """
+        if self.pickling:
+            iterable = map(dumps, iterable)
         self.redis.rpush(self.key_name, *iterable)
 
     def remove(self, value):
@@ -36,16 +69,24 @@ class RedisList(object):
         L.remove(value) -- remove first occurrence of value.
         Raises ValueError if the value is not present.
         """
+        if self.pickling:
+            value = dumps(value)
         if not self.redis.lrem(self.key_name, value, 1):
             raise ValueError('value not in list')
 
     def __getitem__(self, index):
         """ x.__getitem__(y) <==> x[y] """
         if isinstance(index, int):
-            return self.redis.lindex(self.key_name, index)
+            item = self.redis.lindex(self.key_name, index)
+            if self.pickling:
+                item = loads(item)
+            return item
         elif isinstance(index, slice):
             start, stop, step = index.indices(len(self))
-            return self.redis.lrange(self.key_name, start, stop)
+            item = self.redis.lrange(self.key_name, start, stop)
+            if self.pickling:
+                item = map(loads, item)
+            return item
         else:
             raise TypeError('Invalid index type')
 
--- tests/test_list.py
+++ tests/test_list.py
@@ -5,42 +5,57 @@ from bindings import RedisList
 
 class TestRedisList(object):
 
-    def test_values(self, r):
-        r_list = RedisList(r, 'a', 1)
-        assert r_list.values == ['1']
+    def test_not_iterable(self, r):
+        with pytest.raises(ValueError):
+            RedisList(r, 'a', 1)
+
+    def test_wrong_type(self, r):
+        r.hset('a', 1, 1)
+        with pytest.raises(TypeError):
+            RedisList(r, 'a')
+
+    def test_new_values(self, r):
+        r_list = RedisList(r, 'a', [1])
+        assert r_list.values == [1]
+
+    def test_old_values(self, r):
+        r_list = RedisList(r, 'a', [1])
+        r_list_2 = RedisList(r, 'a')
+        assert r_list.values == r_list_2.values
+        assert r_list != r_list_2
 
     def test_append(self, r):
         r_list = RedisList(r, 'a')
         r_list.append(1)
-        assert r_list.values == ['1']
+        assert r_list.values == [1]
 
     def test_extend(self, r):
         r_list = RedisList(r, 'a')
         r_list.extend([1, 2])
-        assert r_list.values == ['1', '2']
+        assert r_list.values == [1, 2]
 
     def test_remove(self, r):
-        r_list = RedisList(r, 'a', 1, 2, 1)
-        r_list.remove('1')
-        assert r_list.values == ['2', '1']
+        r_list = RedisList(r, 'a', [1, 2, 1])
+        r_list.remove(1)
+        assert r_list.values == [2, 1]
         with pytest.raises(ValueError):
-            r_list.remove('3')
+            r_list.remove(3)
 
     def test_get_item(self, r):
-        r_list = RedisList(r, 'a', 1, 2, 1)
-        assert r_list[1] == '2'
-        assert r_list[1:2] == ['2', '1']
+        r_list = RedisList(r, 'a', [1, 2, 1])
+        assert r_list[1] == 2
+        assert r_list[1:2] == [2, 1]
         with pytest.raises(TypeError):
             r_list['a']
 
     def test_len(self, r):
-        r_list = RedisList(r, 'a', 1)
+        r_list = RedisList(r, 'a', [1])
         assert len(r_list) == 1
 
     def test_iter(self, r):
-        r_list = RedisList(r, 'a', 1)
-        assert list(r_list) == ['1']
+        r_list = RedisList(r, 'a', [1])
+        assert list(r_list) == [1]
 
     def test_repr(self, r):
-        r_list = RedisList(r, 'a', 1)
-        assert str(r_list) == 'RedisList: [\'1\']'
+        r_list = RedisList(r, 'a', [1])
+        assert str(r_list) == 'RedisList: [1]'
