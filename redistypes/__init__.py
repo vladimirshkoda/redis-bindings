f71211837c2b4f9ab6d18c1b8e50daaf6d2332ca
Vladimir Shkoda <vladimir.shkoda.51@gmail.com>
2018-11-05 04:46:06 +0300

    Fix relative import.

--- redistypes/__init__.py
+++ redistypes/__init__.py
@@ -1,4 +1,4 @@
 from .bindings import RedisList
-from descriptors import IRedisField, IRedisListField
+from .descriptors import IRedisField, IRedisListField
 
 __all__ = [RedisList, IRedisField, IRedisListField]
