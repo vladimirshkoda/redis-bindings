e5849526917cac943a7ae8baca5aa7ae7fc8136d
Vladimir Shkoda <vladimir.shkoda.51@gmail.com>
2019-03-15 17:44:17 +0300

    Adapt the example to wemake style guide.

--- example.py
+++ example.py
@@ -1,30 +1,43 @@
-from redis import Redis
-from redistypes.descriptors import IRedisField, IRedisListField
+"""An example of the Redis descriptors usage."""
 
+from redis import Redis
+
+from redistypes.descriptors import IRedisField, IRedisListField
 
 r_connection = Redis()
 
 
 class RedisField(IRedisField):
+    """IRedisField implementation."""
+
     def __init__(self, pickling=True):
-        super(RedisField, self).__init__(
+        """Set `r_connection` as the Redis connection pool by default."""
+        super().__init__(
             redis_connection=r_connection,
-            pickling=pickling
+            pickling=pickling,
         )
 
     def get_key_name(self, instance):
+        """
+        Return Redis key name of the attribute.
+
+        It enforces instance using this descriptor to have the `pk` attribute.
+        """
         return ':'.join([
-            instance.__class__.__name__, str(instance.pk), self.name
+            instance.__class__.__name__, str(instance.pk), self.name,
         ])
 
 
 class RedisListField(IRedisListField, RedisField):
-    pass
+    """IRedisListField implementation."""
 
 
-class Student:
+class Student(object):
+    """Casual model of student."""
+
     name = RedisField()
     subjects = RedisListField()
 
     def __init__(self, pk):
+        """Student instance has to be initialized with a primary key `pk`."""
         self.pk = pk
