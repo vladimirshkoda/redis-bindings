babd591eef98e7bc9e5cc2b520e38ee6ce314c56
Vladimir Shkoda <vladimir.shkoda.51@gmail.com>
2019-03-28 21:07:35 +0300

    Provide an explicit index of the package.

--- redistypes/__init__.py
+++ redistypes/__init__.py
@@ -4,3 +4,13 @@ Redis native types for Python.
 Redis bindings is an attempt to bring Redis types into Python as native ones. It
 is based on redis-py and includes RedisList, IRedisField and IRedisListField.
 """
+
+from .bindings import RedisDict, RedisList
+from .descriptors import IRedisField, IRedisListField
+
+__all__ = [
+    'RedisList',
+    'RedisDict',
+    'IRedisField',
+    'IRedisListField',
+]
